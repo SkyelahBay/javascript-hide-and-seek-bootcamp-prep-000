6ffd280679fb2b1ce6497b61b2832b002bc83bb5
Skyelah Hanna Bay <skyelahbay@gmail.com>
2018-04-17 23:46:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,24 +18,10 @@ function increaseRankBy(n){
 
 
 function deepestChild(){
-  let arr = document.querySelector('div#grand-node').querySelectorAll('div');
+  let arr = document.querySelector('div#grand-node');
   let current = arr;
   let next = [];
   
-  let check = n => n.firstChild; //if false it would return true so it would return that one.
-
-  while(current){
-    if(!check(current)){
-      return current;
-    }
-    if(Array.isArray(current)){
-      for(var i = 0; i < current.length; i++){
-        next.push(current[i]);
-      }
-    }
-    current = next.shift();
-  }
-  return null;
   
 }
 
